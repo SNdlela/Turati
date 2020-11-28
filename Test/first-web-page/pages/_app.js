203034f1601bb49cf11fc5b4e74e5788a714d19e
SNdlela <sandilelndlela@gmail.com>
2020-11-28 22:27:18 +0200

    Fix of form and funcional

--- Test/first-web-page/pages/_app.js
+++ Test/first-web-page/pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
-
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
+</style>
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- Test/first-web-page/pages/index.js
+++ Test/first-web-page/pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
+import React, { useState } from 'react';
 import Link from 'next/link'
 
 export default function Home() {
+  const [textValue, setTextValue] = useState("");
+  const [displayValue, setDisplayValue] = useState("");
   return (
     <>
         <Head>
@@ -10,25 +13,25 @@ export default function Home() {
       <h1>Welcome, your future awaits!</h1>
 
       <iframe src="https://www.youtube.com/embed/Xo1pEPoYi8Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>
-      <form id="detailsForm">
+      <div id="detailsForm">
         <label for="name">Name:</label>
-        <input type="text" id ="name" placeholder="Enter name"></input><br></br>
-      </form>
+        <input type="text" 
+        name ="myInput" 
+        placeholder="Enter name" 
+        onChange={(e)=>setTextValue(e.target.value)}
+        >
+        </input><br></br>
+        <button onClick={()=>setDisplayValue(textValue)}>Submit </button>
+      </div>
 
       {/* <Link href="/sub"> */}
-      <button >Submit </button>
+      
       {/* </Link> */}
-      <p id="display"></p>
+      <p id="display">Hello {displayValue}, nice to meet you!</p>
 
     </>
   )
 }
-function processData()
-{
-  // var temp = document.getElementById("detailsForm");
-  // var name = temp.elements[0].value;
-  // document.getElementById("display").innerHTML = name;
 
-}
 
 
