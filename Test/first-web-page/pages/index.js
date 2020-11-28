c8031fa0535e8d296e56c5821fc6e8d8edb6100a
SNdlela <sandilelndlela@gmail.com>
2020-11-28 19:54:41 +0200

    Modification of static properties

--- Test/first-web-page/pages/index.js
+++ Test/first-web-page/pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 export default function Home() {
-
   return (
     <>
         <Head>
@@ -10,15 +9,26 @@ export default function Home() {
         </Head>
       <h1>Welcome, your future awaits!</h1>
 
-      <iframe src="https://www.youtube.com/embed/Xo1pEPoYi8Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <form>
+      <iframe src="https://www.youtube.com/embed/Xo1pEPoYi8Y" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ></iframe>
+      <form id="detailsForm">
         <label for="name">Name:</label>
-        <input type="text" id ="name"></input><br></br>
-        <Link href="/sub">
-        <input type="submit" value="Submit"></input>
-        </Link>
-        
+        <input type="text" id ="name" placeholder="Enter name"></input><br></br>
       </form>
+
+      {/* <Link href="/sub"> */}
+      <button >Submit </button>
+      {/* </Link> */}
+      <p id="display"></p>
+
     </>
   )
 }
+function processData()
+{
+  // var temp = document.getElementById("detailsForm");
+  // var name = temp.elements[0].value;
+  // document.getElementById("display").innerHTML = name;
+
+}
+
+
